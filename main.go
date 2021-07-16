d69df580de833e32ec26ebc23c4721bd270cae07
DesertFox <cerealbh@gmail.com>
2021-07-16 12:33:59 -0500

    This commit adds VimInstallPlug() method for downloading and installing plug.vim 	modified:   main.go

--- main.go
+++ main.go
@@ -3,46 +3,67 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
+	"net/http"
 	"os"
 )
 
 func main() {
-	_, err := installVim()
-	if err != nil {
-		panic(err.Error())
-	}
+	installVim()
+	installVimPlug()
 }
 
-func installVim() (int64, error) {
-	workdir, err := os.Getwd()
-	if err != nil {
-		return 0, err
-	}
-	sourceVimrc := workdir + "/dot-files/.vimrc"
+func installVim() {
+	sourceVimrc, err := os.Getwd()
+	check(err)
+	sourceVimrc = sourceVimrc + "/dot-files/.vimrc"
 
 	source, err := os.Open(sourceVimrc)
-	if err != nil {
-		return 0, err
-	}
+	check(err)
 	defer source.Close()
 
 	dst := os.Getenv("HOME") + "/.vimrc"
 	if _, err := os.Stat(dst); err == nil {
 		fmt.Printf("vimrc file already exists\n")
-		return 0, err
+		return
 	}
 
 	destination, err := os.Create(dst)
-	if err != nil {
-		return 0, err
-	}
+	check(err)
 	defer destination.Close()
 
-	nBytes, err := io.Copy(destination, source)
-	if err != nil {
-		return 0, err
-	}
+	_, err = io.Copy(destination, source)
+	check(err)
+
 	fmt.Printf("Installed vimrc\n")
 
-	return nBytes, err
+	return
+}
+
+func installVimPlug() {
+	const VimPlugUrl = "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+	var VimAutoloadPath = os.Getenv("HOME") + "/.vim/autoload/plug.vim"
+
+	resp, err := http.Get(VimPlugUrl)
+	check(err)
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	check(err)
+
+	if _, err := os.Stat(VimAutoloadPath); err == nil {
+		fmt.Printf("vim plug.vim file already exists\n")
+		return
+	}
+
+	err = ioutil.WriteFile(VimAutoloadPath, body, 0644)
+	check(err)
+
+	return
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
