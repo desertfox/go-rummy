e87524f499b66e80b22a006077c71395e5322958
DesertFox <cerealbh@gmail.com>
2021-07-20 17:01:56 -0500

    Removed intermediary variables and added new structs for bash configs 	modified:   go-rummy 	modified:   main.go

--- main.go
+++ main.go
@@ -10,60 +10,94 @@ import (
 	"strings"
 )
 
+type RummyBash struct {
+	bashAliases string
+	overwrite   *bool
+}
+
 type RummyVim struct {
 	vimrc, vimPlugUrl, vimPlugDestFile string
+	overwrite                          *bool
 }
 
 type RummyConfig struct {
-	cwd, dotfilesName, bashAliases string
-	rVim                           RummyVim
+	cwd, dotfilesName string
+	rBash             RummyBash
+	rVim              RummyVim
 }
 
 var Config RummyConfig
 
 func main() {
+	fmt.Println("Main")
 
-	var (
-		overwriteVimrc     = flag.Bool("ovimrc", false, "Overwrite .vimrc file")
-		overwriteBashAlias = flag.Bool("obashalias", false, "Overwrite .bash_aliases file")
-	)
-
-	flag.Parse()
-
-	installBash(overwriteBashAlias)
-
-	installVimrc(overwriteVimrc)
-
-	installVimPlug()
+	installBash()
+	installVim()
 }
 
 func init() {
 	fmt.Println("Init")
 
-	var (
-		dotfiles        = "dot-files"
-		bashAliases     = ".bash_aliases"
-		vimrc           = ".vimrc"
-		vimPlugUrl      = "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
-		vimPlugDestFile = "/.vim/autoload/plug.vim"
-	)
-
 	cwd, err := os.Getwd()
 	check(err)
 
-	Config = RummyConfig{cwd, dotfiles, bashAliases, RummyVim{vimrc, vimPlugUrl, vimPlugDestFile}}
+	Config = RummyConfig{
+		cwd:          cwd,
+		dotfilesName: "dot-files",
+		rBash: RummyBash{
+			bashAliases: ".bash_aliases",
+			overwrite:   flag.Bool("oba", false, "Overwrite .bash_aliases file")},
+		rVim: RummyVim{
+			vimrc:           ".vimrc",
+			vimPlugUrl:      "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim",
+			vimPlugDestFile: ".vim/autoload/plug.vim",
+			overwrite:       flag.Bool("ovrc", false, "Overwrite .vimrc file")}}
+
+	flag.Parse()
 }
 
-func installBash(overwriteBashAlias *bool) {
+func installVim() {
+	fmt.Println("installVim")
+
+	installVimrc()
+
+	installVimPlug()
+}
+
+func installVimrc() {
+	fmt.Println("installVimrc")
+
+	initConfigFile(Config.rVim.vimrc, Config.rVim.vimrc, Config.rVim.overwrite)
+}
+
+func installVimPlug() {
+	var vimAutoloadPath = strings.Join([]string{os.Getenv("HOME"), Config.rVim.vimPlugDestFile}, "/")
+
+	if _, err := os.Stat(vimAutoloadPath); err == nil {
+		fmt.Printf("vim plug.vim file already exists. bailing\n")
+		return
+	}
+
+	resp, err := http.Get(Config.rVim.vimPlugUrl)
+	check(err)
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	check(err)
+	err = ioutil.WriteFile(vimAutoloadPath, body, 0644)
+	check(err)
+}
+
+func installBash() {
 	fmt.Println("installBash")
 
-	installBashAliases(overwriteBashAlias)
+	installBashAliases(Config.rBash.overwrite)
 }
 
 func installBashAliases(overwriteBashAliases *bool) {
 	fmt.Println("installBashAliases")
 
-	initConfigFile(Config.bashAliases, Config.bashAliases, overwriteBashAliases)
+	initConfigFile(Config.rBash.bashAliases, Config.rBash.bashAliases, Config.rBash.overwrite)
 }
 
 func initConfigFile(source string, dest string, overwrite *bool) {
@@ -93,30 +127,6 @@ func initConfigFile(source string, dest string, overwrite *bool) {
 	check(err)
 
 	fmt.Printf("Installed %v bytes:%v\n", dest, bytesCopied)
-}
-
-func installVimrc(ovimrc *bool) {
-	fmt.Println("installVimrc")
-
-	initConfigFile(Config.rVim.vimrc, Config.rVim.vimrc, ovimrc)
-}
-
-func installVimPlug() {
-	var vimAutoloadPath = strings.Join([]string{os.Getenv("HOME"), Config.rVim.vimPlugDestFile}, "/")
-
-	if _, err := os.Stat(vimAutoloadPath); err == nil {
-		fmt.Printf("vim plug.vim file already exists. bailing\n")
-		return
-	}
-
-	resp, err := http.Get(Config.rVim.vimPlugUrl)
-	check(err)
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	check(err)
-	err = ioutil.WriteFile(vimAutoloadPath, body, 0644)
-	check(err)
 }
 
 func check(e error) {
